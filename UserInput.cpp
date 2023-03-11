9fb432139268e07c9a9e8e85546ca8d086fa9eb5
Mubashar Yasin <127579054+iamMubasharYasin@users.noreply.github.com>
2023-03-10 20:31:30 -0800

    Create UserInput.cpp

---
+++ UserInput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueone;
+	int valuetwo;
+	
+	cout<<"Please enter value one";
+	cin>>valueone;
+	
+	cout<<"Please enter value two";
+	cin>>valuetwo;
+	
+	cout<<valueone+valuetwo;
+}
